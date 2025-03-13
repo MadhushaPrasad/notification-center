399a8b094e7db4641a23d510d04832b31a631371
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-13 11:00:52 +0530

    feat: add NotificationOptions interface for customizable notification settings

---
+++ src/core/interfaces/models/NotificationOptions.ts
@@ -0,0 +1,21 @@
+export default interface NotificationOptions {
+  title?: string;
+  text?: string;
+  type: string;
+  toast?: boolean;
+  position?: string;
+  duration?: number;
+  icon?: string;
+  alertColor?: string;
+  alertWidth?: string;
+  alertHeight?: string;
+  showCloseButton?: boolean;
+  showCancelButton?: boolean;
+  showConfirmButton?: boolean;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+  confirmButtonColor?: string;
+  cancelButtonColor?: string;
+  confirmButtonClass?: string;
+  cancelButtonClass?: string;
+}
