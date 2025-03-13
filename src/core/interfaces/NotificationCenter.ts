ffa2fda0f5aab2ee71bafc8f2617586d1a6e5110
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-13 14:49:28 +0530

    refactor: remove unused NotificationOptions methods from NotificationCenter interface

--- src/core/interfaces/NotificationCenter.ts
+++ src/core/interfaces/NotificationCenter.ts
@@ -1,10 +1,3 @@
-import NotificationOptions from '../interfaces/models/NotificationOptions';
-
 export default interface NotificationCenter {
   closeNotification(notification: HTMLElement): void;
-
-  success(options: NotificationOptions): void;
-  error(options: NotificationOptions): void;
-  warning(options: NotificationOptions): void;
-  show(options: NotificationOptions): void;
 }
--- src/core/interfaces/custom/ToastNotification.ts
+++ src/core/interfaces/custom/ToastNotification.ts
@@ -1,6 +1,11 @@
 import NotificationCenter from './../NotificationCenter';
 import NotificationOptions from './../models/NotificationOptions';
 
-export default interface ToastNotification extends NotificationCenter{
+export default interface ToastNotification extends NotificationCenter {
   showNotification(options: NotificationOptions): void;
+
+  success(options: NotificationOptions): void;
+  error(options: NotificationOptions): void;
+  warning(options: NotificationOptions): void;
+  show(options: NotificationOptions): void;
 }
