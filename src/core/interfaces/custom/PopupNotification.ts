10733ec06cbb85f747dc8288617affdb6be75ff5
Madhusha Prasad <madushaprasad21@gmail.com>
2025-03-13 15:00:41 +0530

    chore: change functions as promise type

--- src/core/interfaces/custom/PopupNotification.ts
+++ src/core/interfaces/custom/PopupNotification.ts
@@ -1,6 +1,11 @@
 import NotificationCenter from './../NotificationCenter';
 import NotificationOptions from './../models/NotificationOptions';
 
-export default interface PopupNotification extends NotificationCenter{
-  showPopup(options: NotificationOptions): void;
+export default interface PopupNotification extends NotificationCenter {
+  showPopup(options: NotificationOptions, resolve?: (value: boolean) => void): void
+
+  success(options: NotificationOptions): Promise<boolean>;
+  error(options: NotificationOptions): Promise<boolean>;
+  warning(options: NotificationOptions): Promise<boolean>;
+  show(options: NotificationOptions): Promise<boolean>;
 }
